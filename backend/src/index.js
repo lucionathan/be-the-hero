ebe880c19d9b0b6c9936117264397562696cee47
lucionathan <lucio.matias@ccc.ufcg.edu.br>
2020-03-27 16:28:41 -0300

    autorização feita

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const cors = require('cors');
 
+const { errors } = require('celebrate');
+
 const routes = require('./routes');
 
 const app = express();
@@ -10,5 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333, () => { console.log("listening on port 3333"); });
